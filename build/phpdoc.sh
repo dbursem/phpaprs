9518536bc46f16e60293dc2e0f5d85f1f0e27f98
David <david@dbursem.nl>
2015-10-28 19:17:48 +0100

    Delete phpdoc.sh

--- build/phpdoc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PWD=`pwd`
-
-if [ `basename $PWD` == "build" ]; then
-	echo "Wrong directory, run this from parent directory.";
-	exit;
-fi
-
-phpdoc -d lib -f \*.php -t docs -ti "phpAPRS Class Documentation" -s 
